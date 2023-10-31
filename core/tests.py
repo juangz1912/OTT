591ccf3de66323521512f0b97578e8ac808f758a
JuanGomez-14 <jmgomezc14@gmail.com>
2023-10-30 21:51:31 -0500

    Se agregan las pruebas doubles con Mock

--- core/tests.py
+++ core/tests.py
@@ -3,21 +3,25 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from django.forms import ValidationError
 from .models import Item, Item2, Item3, Item4
+from unittest.mock import MagicMock, patch
+
 
 class LoginViewTestCase(TestCase):
     def setUp(self):
         # Crea un usuario de prueba
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword')
 
     def test_login_page_loads_properly(self):
         # Prueba que la página de inicio de sesión cargue correctamente
-        response = self.client.get(reverse('login')) 
+        response = self.client.get(reverse('login'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'core/templates/registration/login.html')
+        self.assertTemplateUsed(
+            response, 'core/templates/registration/login.html')
 
     def test_login_form(self):
         # Prueba que el formulario de inicio de sesión esté presente en la página
-        response = self.client.get(reverse('login')) 
+        response = self.client.get(reverse('login'))
         self.assertContains(response, '<form')
         self.assertContains(response, 'name="username"')
         self.assertContains(response, 'name="password"')
@@ -25,23 +29,28 @@ class LoginViewTestCase(TestCase):
 
     def test_successful_login(self):
         # Prueba el inicio de sesión exitoso
-        response = self.client.post(reverse('login'), {'username': 'testuser', 'password': 'testpassword'})
-        self.assertEqual(response.status_code, 302) 
+        response = self.client.post(
+            reverse('login'), {'username': 'testuser', 'password': 'testpassword'})
+        self.assertEqual(response.status_code, 302)
 
     def test_failed_login(self):
         # Prueba el inicio de sesión fallido con credenciales incorrectas
-        response = self.client.post(reverse('login'), {'username': 'testuser', 'password': 'incorrectpassword'})
+        response = self.client.post(
+            reverse('login'), {'username': 'testuser', 'password': 'incorrectpassword'})
         self.assertEqual(response.status_code, 200)
-        self.assertFormError(response, 'form', None, 'Por favor, introduzca un nombre de usuario y contraseña válidos.')
+        self.assertFormError(
+            response, 'form', None, 'Por favor, introduzca un nombre de usuario y contraseña válidos.')
 
     def test_non_field_errors(self):
         # Prueba que los errores no relacionados con campos se muestren correctamente
-        response = self.client.post(reverse('login'), {'username': 'testuser', 'password': 'incorrectpassword'})
-        self.assertContains(response, 'Por favor, introduzca un nombre de usuario y contraseña válidos.')
+        response = self.client.post(
+            reverse('login'), {'username': 'testuser', 'password': 'incorrectpassword'})
+        self.assertContains(
+            response, 'Por favor, introduzca un nombre de usuario y contraseña válidos.')
 
     def test_csrf_token(self):
         # Prueba que el token CSRF esté presente en el formulario
-        response = self.client.get(reverse('login')) 
+        response = self.client.get(reverse('login'))
         self.assertContains(response, 'csrfmiddlewaretoken')
 
     def test_links(self):
@@ -49,11 +58,14 @@ class LoginViewTestCase(TestCase):
         response = self.client.get(reverse('login'))
         self.assertContains(response, reverse('register'))
 
+
 class RegisterViewTestCase(TestCase):
     def test_register_page_loads_properly(self):
         response = self.client.get(reverse('register'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'core/templates/registration/register.html')  # Ajusta la ruta de la plantilla aquí
+        # Ajusta la ruta de la plantilla aquí
+        self.assertTemplateUsed(
+            response, 'core/templates/registration/register.html')
 
     def test_register_form(self):
         response = self.client.get(reverse('register'))
@@ -76,7 +88,8 @@ class RegisterViewTestCase(TestCase):
             'password2': 'testpassword',
         }
         response = self.client.post(reverse('register'), data)
-        self.assertEqual(response.status_code, 302)  # Debería redirigir después de un registro exitoso
+        # Debería redirigir después de un registro exitoso
+        self.assertEqual(response.status_code, 302)
 
     def test_failed_registration(self):
         data = {
@@ -88,8 +101,10 @@ class RegisterViewTestCase(TestCase):
             'password2': 'differentpassword',  # Contraseña diferente
         }
         response = self.client.post(reverse('register'), data)
-        self.assertEqual(response.status_code, 200)  # Debería volver a la página de registro
-        self.assertFormError(response, 'form', 'password2', 'Las contraseñas no coinciden.')
+        # Debería volver a la página de registro
+        self.assertEqual(response.status_code, 200)
+        self.assertFormError(response, 'form', 'password2',
+                             'Las contraseñas no coinciden.')
 
     def test_user_created(self):
         data = {
@@ -114,7 +129,8 @@ class RegisterViewTestCase(TestCase):
             'password2': 'testpassword',
         }
         response = self.client.post(reverse('register'), data)
-        self.assertEqual(self.client.session['_auth_user_id'], str(User.objects.get(username='testuser').id))
+        self.assertEqual(self.client.session['_auth_user_id'], str(
+            User.objects.get(username='testuser').id))
 
     def test_redirect_after_successful_registration(self):
         data = {
@@ -126,7 +142,8 @@ class RegisterViewTestCase(TestCase):
             'password2': 'testpassword',
         }
         response = self.client.post(reverse('register'), data, follow=True)
-        self.assertEqual(response.status_code, 200)  # Verifica que redirija a la página de inicio o a donde corresponda
+        # Verifica que redirija a la página de inicio o a donde corresponda
+        self.assertEqual(response.status_code, 200)
 
 
 class ProductViewTestCase(TestCase):
@@ -140,7 +157,59 @@ class ProductViewTestCase(TestCase):
 
     def test_product_page_loads_properly(self):
         # Prueba que la página de productos cargue correctamente
-        response = self.client.get(reverse('products')) 
+        response = self.client.get(reverse('products'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'core/core/products.html') 
+        self.assertTemplateUsed(response, 'core/core/products.html')
 
+#
+#
+# PRUEBAS CON MOCK
+#
+#
+
+
+class LoginViewTestCaseMock(TestCase):
+    def setUp(self):
+        # Crea un usuario de prueba
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword')
+
+    def test_successful_login(self):
+        # Prueba el inicio de sesión exitoso
+        with patch('django.contrib.auth.authenticate') as mock_authenticate:
+            # Configura el mock para que devuelva el usuario de prueba
+            mock_authenticate.return_value = self.user
+            response = self.client.post(
+                reverse('login'), {'username': 'testuser', 'password': 'testpassword'})
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_failed_login(self):
+        # Prueba el inicio de sesión fallido con credenciales incorrectas
+        with patch('django.contrib.auth.authenticate') as mock_authenticate:
+            # Configura el mock para que devuelva None, simulando un inicio de sesión fallido
+            mock_authenticate.return_value = None
+            response = self.client.post(
+                reverse('login'), {'username': 'testuser', 'password': 'incorrectpassword'})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertFormError(
+            response, 'form', None, 'Por favor, introduzca un nombre de usuario y contraseña válidos.')
+
+
+class RegisterViewTestCaseMock(TestCase):
+    def test_successful_registration(self):
+        data = {
+            'username': 'testuser',
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email': 'test@example.com',
+            'password1': 'testpassword',
+            'password2': 'testpassword',
+        }
+        # Simula el envío del formulario de registro
+        with patch('django.contrib.auth.models.User.objects.create_user'):
+            response = self.client.post(reverse('register'), data)
+
+        # Debería redirigir después de un registro exitoso
+        self.assertEqual(response.status_code, 302)
